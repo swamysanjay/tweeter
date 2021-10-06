ac98d9a230d522d37106501cdeea2fc238f4a5c5
Sanjay Swamy <swamysanjay98@gmail.com>
2021-10-06 20:38:09 +0000

    Day 2 of creating our twitter clone. Includes resyling of Nav bar and creating a general tweets section

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,21 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
+const createTweetElement = function(tweet) {
+  const $tweet = $(`<article class="tweet">Hello world</article>`);
+
+  
+}
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  const maxChar = 140;
+  let result = 0;
+
+  $(".new-tweet textarea").on("input", function() {
+    let counter = $(this).val().length;
+    const element = $(".counter")
+    result = maxChar - counter;
+    //(counter).text(maxChar - $(this).val().length);
+    if (counter > maxChar) {
+      element.addClass('over');
+    } else {
+      element.removeClass('over');
+    }
+    /*if ($(this).val().length > maxChar) {
+      $($counter).addClass('over');
+    }
+    if ($(this).val().length <= maxChar) {
+      $($counter).removeClass('over');
+    } */
+    element.text(result);
+    console.log(element);
+  });
+  // --- our code goes here ---
+});

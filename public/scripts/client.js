e49c01d1a8f71e5d99fe440962eab3eb301290c2
Sanjay Swamy <swamysanjay98@gmail.com>
2021-10-07 18:12:27 +0000

    Created createTweetElement function that creates a tweet with the same CSS style as other tweets, and created renderTweet function that goes through array of tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,22 +3,51 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-
-const tweetData = {
+const tweetData = [{
   "user": {
     "name": "Newton",
     "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+    "handle": "@SirIsaac"
     },
   "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
   "created_at": 1461116232227
-}
+}];
 
-const createTweetElement = function(tweet) {
-  const $tweet = $(`<article class="tweet">Hello world</article>`);
+$(document).ready(() => {
+  const renderTweets = function(tweets) {
+    for (let i of tweets) {
+      const $tweet = createTweetElement(i);
+      $('#tweets-container').append($tweet);
+    }
+  }
 
-  
-}
+  const createTweetElement = (tweet) => {
+    console.log("Hello", tweet);
+    const createdAtTimeAgo = timeago.format(tweet.created_at);
+    return `<article class="tweet">
+    <header>
+      <img class="person-img" src =${tweet.user.avatars} alt="Person">
+      <h3 class="name">${tweet.user.name}</h3>
+      <p class="handle">${tweet.user.handle}</h3>
+    </header>
+    <p>${tweet.content.text}</p>
+    <footer>
+      <p>${createdAtTimeAgo}</p>
+      <div class="tools">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </div>
+    </footer>
+  </article> `
+  }
+
+  renderTweets(tweetData);
+  //const $tweet = createTweetElement(tweetData);
+
+  // Test / driver code (temporary)
+  //console.log($tweet); // to see what it looks like
+  //$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+})

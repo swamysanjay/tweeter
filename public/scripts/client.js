a2bda82601fe27d8b5aa812d728ec5eeb1408252
Sanjay Swamy <swamysanjay98@gmail.com>
2021-10-09 14:06:29 +0000

    Created a form submission function using JQuery and another function that fetches the tweets using Ajax. Validation errors such as submitting an empty tweet was also created using JQuery.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,51 +3,83 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const tweetData = [{
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}];
+
+const createTweetElement = (tweet) => {
+  // eslint-disable-next-line
+  const createdAtTimeAgo = timeago.format(tweet.created_at);
+  return `<article class="tweet">
+  <header>
+    <img class="person-img" src =${tweet.user.avatars} alt="Person">
+    <h3 class="name">${tweet.user.name}</h3>
+    <p class="handle">${tweet.user.handle}</h3>
+  </header>
+  <p class="tweet-text">${tweet.content.text}</p>
+  <footer>
+    <p>${createdAtTimeAgo}</p>
+    <div class="tools">
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+    </div>
+  </footer>
+</article> `;
+};
+
+const renderTweets = function(tweets) {
+  for (let i of tweets) {
+    const $tweet = createTweetElement(i);
+    $('#tweets-container').prepend($tweet);
+  }
+};
 
 $(document).ready(() => {
-  const renderTweets = function(tweets) {
-    for (let i of tweets) {
-      const $tweet = createTweetElement(i);
-      $('#tweets-container').append($tweet);
-    }
-  }
+  const loadTweets = () => {
+    $.get('/tweets')
+      .done((response) => {
+        console.log(response);
+        renderTweets(response);
+      })
+      .fail((error) => {
+        console.log('Something is wrong', error);
+      });
+  };
+  loadTweets();
 
-  const createTweetElement = (tweet) => {
-    console.log("Hello", tweet);
-    const createdAtTimeAgo = timeago.format(tweet.created_at);
-    return `<article class="tweet">
-    <header>
-      <img class="person-img" src =${tweet.user.avatars} alt="Person">
-      <h3 class="name">${tweet.user.name}</h3>
-      <p class="handle">${tweet.user.handle}</h3>
-    </header>
-    <p>${tweet.content.text}</p>
-    <footer>
-      <p>${createdAtTimeAgo}</p>
-      <div class="tools">
-        <i class="fas fa-flag"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="fas fa-heart"></i>
-      </div>
-    </footer>
-  </article> `
-  }
+  const submitTweet = function() {
+    const form = $('#tweet-form');
+    form.on('submit', function(event) {
+      event.preventDefault();
+      const dataSerialized = $(this).serialize();
+      const tweetLength = $('#tweet-text').val().length;
+      if (tweetLength === 0 || tweetLength === null) {
+        return $('#error-container').html('<span class="fas fa-exclamation-triangle">Write something in the tweet smh</span>').slideDown('slow');
+        // alert("Write something in the tweet smh");
+      }
+      if (tweetLength > 140) {
+        return $('#error-container').html('<span class="fas fa-exclamation-triangle">Fam you wrote too much take it easy big mans</span>').slideDown("slow");
+        // return alert("Fam you wrote too much take it easy big mans");
+      }
+      $.post('/tweets', dataSerialized)
+        .done(() => {
+          loadTweets();
+          console.log('Success');
+        })
+        .fail(() => {
+          console.log("Failed");
+        });
+      return $('#error-container').slideUp('slow');
+    });
+  };
+  submitTweet();
+  
+  
+  
+ 
+  
+});
 
-  renderTweets(tweetData);
-  //const $tweet = createTweetElement(tweetData);
+// const $tweet = createTweetElement(tweetData);
 
-  // Test / driver code (temporary)
-  //console.log($tweet); // to see what it looks like
-  //$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-})
+// Test / driver code (temporary)
+//console.log($tweet); // to see what it looks like
+//$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
